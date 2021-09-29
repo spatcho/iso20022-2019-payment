e6e34146c81e0af1257cf69a605c36898b256d4c
sonkan <alexander.k.gertz@gmail.com>
2021-09-29 12:53:37 +0200

    Add missing files

---
+++ tests/Z38/SwissPayment/Tests/Money/MixedTypeTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Z38\SwissPayment\Tests\Money;
+
+use Z38\SwissPayment\Money;
+use Z38\SwissPayment\Tests\TestCase;
+
+class MixedTypeTest extends TestCase
+{
+    /**
+     * @covers \Z38\SwissPayment\Money\MixedType::plus
+     */
+    public function testPlus()
+    {
+        $sum = new Money\MixedType(0);
+        $sum = $sum->plus(new Money\CHF(2456));
+        $sum = $sum->plus(new Money\CHF(1000));
+        $sum = $sum->plus(new Money\JPY(1200));
+
+        $this->assertEquals('1234.56', $sum->format());
+    }
+
+    /**
+     * @covers \Z38\SwissPayment\Money\MixedType::minus
+     */
+    public function testMinus()
+    {
+        $sum = new Money\MixedType(100);
+        $sum = $sum->minus(new Money\CHF(5000));
+        $sum = $sum->minus(new Money\CHF(99));
+        $sum = $sum->minus(new Money\JPY(300));
+
+        $this->assertEquals('-250.99', $sum->format());
+    }
+}
